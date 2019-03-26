3e8cebf0c637c8b33593386ef88429431a632876
Adam Smith <adam_d_smith@fws.gov>
2019-03-26 11:33:56 -0400

    Update example usage of `aru_forecast`

--- R/example_usage.R
+++ R/example_usage.R
@@ -1,16 +1,23 @@
+if (!requireNamespace("devtools", quietly = TRUE))
+  install.packages("devtools", quiet = TRUE)
+if (!requireNamespace("remotes", quietly = TRUE))
+  install.packages("remotes", quiet = TRUE)
 if (!requireNamespace("nrsmisc", quietly = TRUE))
-  devtools::install_github("adamdsmith/nrsmisc")
+  remotes::install_github("adamdsmith/nrsmisc")
+if (!requireNamespace("pacman", quietly = TRUE))
+  install.packages("pacman", quiet = TRUE)
 pacman::p_load(nrsmisc, dplyr)
-source("R/aru_forecast.R")
-source("R/aru_sequences.R")
+devtools::source_url("https://raw.githubusercontent.com/adamdsmith/BLRA_ARUs/master/R/aru_forecast.R")
 
 # Cedar Island
+# Get forecast for next week
 aru_forecast(34.9417, -76.34)
-seqs <- aru_sequences(34.9417, -76.34, start = "2019-04-15")
-View(seqs)
 
 # Swanquarter
 aru_forecast(35.43, -76.376)
+
+# Alligator River
+aru_forecast(35.8854, -75.7612)
 
 # St. Marks
 aru_forecast(30.08, -84.159)
